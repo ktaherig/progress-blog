c47f6bca85732ebfa7146eec793f73b19e4058d2
ktaherig <k.taheri.g@protonmail.com>
2020-02-23 17:17:48 -0500

    basic initial setup

--- server/main/app.js
+++ server/main/app.js
@@ -4,14 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// CURRENTLY UNNECESSARY CODE
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
+// CURRENTLY UNNECESSARY CODE
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,9 +21,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// CURRENTLY UNNECESSARY CODE
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
+/**
+ * currently unneeded code
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -37,5 +42,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+*/
 
 module.exports = app;
