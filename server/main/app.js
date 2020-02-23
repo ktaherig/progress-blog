bf7acbecdc6eed0d5fd643cf3eca74c932db6e44
ktaherig <k.taheri.g@protonmail.com>
2020-02-23 17:52:19 -0500

    basic initial setup

--- server/main/app.js
+++ server/main/app.js
@@ -1,14 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes');
 
 // CURRENTLY UNNECESSARY CODE
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
+// const indexRouter = require('./routes/index');
+// const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // CURRENTLY UNNECESSARY CODE
 // view engine setup
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 // CURRENTLY UNNECESSARY CODE
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
+
+app.use('/', indexRouter);
 
 /**
  * currently unneeded code
---
+++ server/main/routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router(); // allows making HTTP requests
+
+router.get('/frontpage', (req, res) => {
+	res.json("Welcome to the Machine");
+});
+
+module.exports = router;
